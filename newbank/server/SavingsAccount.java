63b596681424765787218a59b5a35fccffbe0f18
adrianlio <al2275@bath.ac.uk>
2021-04-03 23:42:45 +0100

    Support for Account Numbers and Sort Codes
    - Updated Account class constructors to support sort codes and account numbers
    - NewBank.java now includes hardcoded sort code that is assigned to every account
    - NewBank.java has new 'assignAccountNumber' function which checks if a randomly generated account number is already in use and if not, returns a new account number

--- newbank/server/SavingsAccount.java
+++ newbank/server/SavingsAccount.java
@@ -2,8 +2,8 @@ package newbank.server;
 
 public class SavingsAccount extends Account {
 
-    public SavingsAccount(String accountName, double openingBalance) {
-        super(accountName, openingBalance);
+    public SavingsAccount(String accountSortCode, int accountNumber, String accountName, double openingBalance) {
+        super(accountSortCode, accountNumber, accountName, openingBalance);
         this.canPay = false;
         this.canLoan = false;
         this.accountType = "Savings";
