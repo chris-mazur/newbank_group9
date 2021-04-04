b66cc41de4728529088f8780528eb51a81fe6558
Chris <kpm42@bath.ac.uk>
2021-04-05 00:24:51 +0100

    Add Admin field in Customer.java

--- newbank/server/Customer.java
+++ newbank/server/Customer.java
@@ -9,7 +9,16 @@ public class Customer {
 	private ArrayList<Loan> currentLoansOffered; // keep a record of all loans currently offered to other customers
 	private ArrayList<Loan> currentLoansReceived; // keep a record of all outstanding loans to be paid back
 	private String password;
+	private Boolean admin = false;
 	
+	public Boolean getAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(Boolean admin) {
+		this.admin = admin;
+	}
+
 	public Customer(String password) {
 		accounts = new ArrayList<>();
 		currentLoansOffered = new ArrayList<>();
